687e3b8c1d4a5caa6fa846449b7cf3e6092d7114
Salah <ahmed.msalah99@gmail.com>
2025-08-25 20:54:57 +0200

    adding support for deployment env configuration

--- src/stat_collector.c
+++ src/stat_collector.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "conf_io.h"
 #include "stats_io.h"
 
 
+#define APP_NAME "stat_collector"
+
+// Try to locate config file
+const char* find_conf_file() {
+    static char path[64];
+
+    // 1. Check environment override (MYAPP_CONF)
+    const char *def_path = "../conf/defconf.conf";
+    if (def_path && access(def_path, R_OK) == 0) {
+        return def_path;
+    }
+    // 2. Linux/macOS system config
+    snprintf(path, sizeof(path), "/etc/%s/deployconf.conf", APP_NAME);
+    if (access(path, R_OK) == 0) return path;
+
+    return NULL; // not found
+}
 
 int main() {
-    printf("Hello, World!\n");
-    const char *conf_file = "../conf/defconf.conf";
+    const char *conf_file = find_conf_file();
+    if (!conf_file) {
+        fprintf(stderr, "Error: could not locate configuration file.\n");
+        return 1;
+    }
     read_conf(conf_file);   
     process_stats stats = read_stats();
     for (int i = 0; i < TOP_N; i++) {
@@ -14,7 +37,3 @@ int main() {
     }
     return 0; 
 }
-
-
-
-
